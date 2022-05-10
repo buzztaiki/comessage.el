a5e8bf044c77e72a048194e62ed2420656f7b8d2
Taiki Sugawara <buzz.taiki@gmail.com>
2022-05-10 20:51:07 +0900

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # comessage
 
-WIP
+`comessage-mode` is a minor mode that provides message coexistence.
+It will resolve multiple packages competing for message output (e.g. flymake and eldoc).
 
+## Example
+For example, to separate flymake and eldoc messages, you can do as the following configuration:
+
+```lisp
+(comessage-define-advice my/comessage-group-flymake 'flymake)
+(comessage-define-advice my/comessage-group-eldoc 'eldoc)
+
+(advice-add 'flymake-goto-next-error :around #'my/comessage-group-flymake)
+(advice-add 'eldoc-message :around #'my/comessage-group-eldoc)
+(advice-add 'eldoc-minibuffer-message :around #'my/comessage-group-eldoc)
+(comessage-global-mode 1)
+```
 ## License
 GPLv3
--- comessage.el
+++ comessage.el
@@ -23,17 +23,7 @@
 ;; `comessage-mode' is a minor mode that provides message coexistence.
 ;; It will resolve multiple packages competing for message output (e.g. flymake and eldoc).
 
-;; Example:
-;; To separate flymake and eldoc messages, you can do as the following configuration:
-;;
-;; 	(comessage-define-advice my/comessage-group-flymake 'flymake)
-;; 	(comessage-define-advice my/comessage-group-eldoc 'eldoc)
-;; 	
-;; 	(advice-add 'flymake-goto-next-error :around #'my/comessage-group-flymake)
-;; 	(advice-add 'eldoc-message :around #'my/comessage-group-eldoc)
-;; 	(advice-add 'eldoc-minibuffer-message :around #'my/comessage-group-eldoc)
-;;	(comessage-global-mode 1)
-
+;; For more information, please see <https://github.com/buzztaiki/comessage.el>.
 
 ;;; Code:
 
